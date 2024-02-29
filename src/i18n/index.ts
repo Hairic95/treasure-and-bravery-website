fddfc92fcf17fcd52ec1df4cfef31d538a7e5c64
Enrico Rossetti <enrico.rossetti@topgraf.it>
2024-02-29 11:03:00 +0100

    lets try to put only the build fuck it

--- src/i18n/index.ts
+++
@@ -1,7 +0,0 @@
-import en from './en';
-import it from './it';
-
-export default {
-  'en-US': en,
-  'it-IT': it,
-};
--- src/store/main/getters.ts
+++
@@ -1,7 +0,0 @@
-import { GetterTree } from 'vuex';
-import { ITBState } from './state';
-import { StateInterface } from '../index';
-
-const getters: GetterTree<ITBState, StateInterface> = {};
-
-export default getters;
